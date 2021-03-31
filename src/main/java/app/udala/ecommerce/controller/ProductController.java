9bcf7fed9a6df28e98f5f2a208322d94d5b62d22
Leandro Udala <leandroudala@gmail.com>
2021-03-31 16:44:12 -0300

    Added New Product Endpoint

--- src/main/java/app/udala/ecommerce/controller/ProductController.java
+++ src/main/java/app/udala/ecommerce/controller/ProductController.java
@@ -1,17 +1,19 @@
 package app.udala.ecommerce.controller;
 
 import java.net.URI;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,13 +31,13 @@ public class ProductController {
 	@Autowired
 	private ProductRepository productRepository;
 	
-	@Cacheable(value = "get-products")
 	@GetMapping
 	public Page<ProductDto> list(@PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 10) Pageable pageable) {
 		Page<Product> products = productRepository.findAll(pageable);
 		return ProductDto.getPageable(products);
 	}
 	
+	@PostMapping
 	public ResponseEntity<ProductDto> create(@Valid @RequestBody ProductForm form, UriComponentsBuilder uriBuilder) {
 		Product product = form.toProduct();
 		
@@ -43,5 +45,15 @@ public class ProductController {
 		
 		URI uri = uriBuilder.path("/products/{id}").buildAndExpand(product.getId().toString()).toUri();
 		return ResponseEntity.created(uri).body(new ProductDto(product));
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ProductDto> get(@PathVariable Long id){
+		Optional<Product> test = productRepository.findById(id);
+		
+		if (test.isEmpty())
+			return ResponseEntity.notFound().build();
+
+		return ResponseEntity.ok(new ProductDto(test.get()));
 	}
 }
